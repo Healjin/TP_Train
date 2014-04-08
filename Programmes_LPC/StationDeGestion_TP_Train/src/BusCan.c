a08694e21df596a80f4c986526a84f8058018035
RAFAEL MARQUES <rafaeldossantosm@gmail.com>
2014-04-08 21:42:04 +0200

    Fin de la partie calcule BRP

--- Programmes_LPC/StationDeGestion_TP_Train/src/BusCan.c
+++ Programmes_LPC/StationDeGestion_TP_Train/src/BusCan.c
@@ -9,17 +9,29 @@
 
 void Init_BusCan(){
 
-	LPC_SC->PCONP |= 0x1 << 13;
-	LPC_SC->PCLKSEL0 &=~ 0x03 << 26;
+	LPC_SC->PCONP |= 0x1 << 13;			//Power
+	LPC_SC->PCLKSEL0 &=~ 0x03 << 26;	//Peripheral clock
 
-	LPC_PINCON->PINSEL0 |= 0x01;
-	LPC_PINCON->PINSEL0 |= 0x01 << 2;
+	LPC_CAN1->BTR |= 0x1110 << 16;		//TSEG1 = 14
+	LPC_CAN1->BTR |= 0x011 << 20;		//TSEG2 = 3
+	LPC_CAN1->BTR |= 0x0000001000;		//BRP
 
-	LPC_CAN1->MOD &=~ 0x01;  		//Reset Mode
-	LPC_CAN1->MOD &= 0x01 << 1;		//Listen Only Mode
-	LPC_CAN1->MOD &=~ 0x01 << 2;	//Self Test Mode
-	LPC_CAN1->MOD &=~ 0x01 << 3;	//Transmit Priority Mode
-	LPC_CAN1->MOD &=~ 0x01 << 4;	//Sleep Mode
-	LPC_CAN1->MOD &=~ 0x01 << 5;	//Receive Polarity Mode
-	LPC_CAN1->MOD &=~ 0x01 << 7;	//Test Mode
+
+	LPC_PINCON->PINSEL0 |= 0x01;		//RD1
+	LPC_PINCON->PINSEL0 |= 0x01 << 2;	//TD1
+
+	LPC_CAN1->MOD &=~ 0x01;  			//Reset Mode
+	LPC_CAN1->MOD &= 0x01 << 1;			//Listen Only Mode
+	LPC_CAN1->MOD &=~ 0x01 << 2;		//Self Test Mode
+	LPC_CAN1->MOD &=~ 0x01 << 3;		//Transmit Priority Mode
+	LPC_CAN1->MOD &=~ 0x01 << 4;		//Sleep Mode
+	LPC_CAN1->MOD &=~ 0x01 << 5;		//Receive Polarity Mode
+	LPC_CAN1->MOD &=~ 0x01 << 7;		//Test Mode
+}
+
+void Write_BusCan(){
+
+}
+void Read_BusCan(){
+
 }
--- Programmes_LPC/StationDeGestion_TP_Train/src/BusCan.h
+++ Programmes_LPC/StationDeGestion_TP_Train/src/BusCan.h
@@ -11,8 +11,10 @@
 
 #include "LPC17xx.h"
 #include <cr_section_macros.h>
+#define BRP 4					//BR = CAN_CLK/(BRP + 1)(20)
 
 void Init_BusCan();
-
+void Write_BusCan();
+void Read_BusCan();
 
 #endif /* BusCan_h_ */
