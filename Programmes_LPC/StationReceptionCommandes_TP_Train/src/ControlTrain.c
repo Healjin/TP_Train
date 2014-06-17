b8b0b6f651a4204fc6d7a844425e4e4ed7719071
Terminatoss <rafael.dossantos.m@gmail.com>
2014-06-17 08:44:18 +0000

    ControlTrain.c edited online with Bitbucket

--- Programmes_LPC/StationReceptionCommandes_TP_Train/src/ControlTrain.c
+++ Programmes_LPC/StationReceptionCommandes_TP_Train/src/ControlTrain.c
@@ -1,14 +1,21 @@
 /*
 *@file ControlTrain.c
-*@author Swagteam
+*@author Rafael Marques
 *@version 1.0
-*@date 1 April 2014
-*@brief BusCan configuration
+*@date 10 mai 2014
+*@brief Affect value of transmission
 */
 
 #include "ControlTrain.h"
 
-
+/*
+*@brief StopGoTrain
+*@param str_bus
+*@param			uint32_t id_Bus
+				uint8_t dlc
+				uint8_t data[8]
+*@param			int statu
+*/
 void StopGoTrain(str_bus* str,int statu){
 	str->id_Bus = 0x00004711;
 	str->dlc = 0x5;
@@ -21,6 +28,15 @@ void StopGoTrain(str_bus* str,int statu){
 
 	str->data[4] = statu;
 }
+/*
+*@brief ChangeDirection
+*@param str_bus
+*@param			uint32_t id_Bus
+				uint8_t dlc
+				uint8_t data[8]
+*@param			int NoTrain
+*@param			int direction
+*/
 void ChangeDirection(str_bus* str,int NoTrain,int direction){
 	str->id_Bus = 0x000A4711;
 	str->dlc = 0x5;
@@ -37,6 +53,15 @@ void ChangeDirection(str_bus* str,int NoTrain,int direction){
 	str->data[6] = 0;
 	str->data[7] = 0;
 }
+/*
+*@brief ChangeSpeed
+*@param str_bus
+*@param			uint32_t id_Bus
+				uint8_t dlc
+				uint8_t data[8]
+*@param			int NoTrain
+*@param			int speed
+*/
 void ChangeSpeed(str_bus* str,int NoTrain,int speed){
 	str->id_Bus = 0x00084711;
 	str->dlc = 0x6;
@@ -53,6 +78,15 @@ void ChangeSpeed(str_bus* str,int NoTrain,int speed){
 	str->data[6] = 0;
 	str->data[7] = 0;
 }
+/*
+*@brief TurnLight
+*@param str_bus
+*@param			uint32_t id_Bus
+				uint8_t dlc
+				uint8_t data[8]
+*@param			int NoTrain
+*@param			int statu
+*/
 void TurnLight(str_bus* str,int statu){
 	str->id_Bus = 0x000C4711;
 	str->dlc = 0x5;
