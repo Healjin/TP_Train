f9c6200b4e27b50d3acd46ab2fc5c49330a67763
mignam <bergerantoineab@gmail.com>
2014-06-19 15:11:20 +0000

    SD.c edited online with Bitbucket

--- Programmes_LPC/StationDeGestion_TP_Train/src/SD.c
+++ Programmes_LPC/StationDeGestion_TP_Train/src/SD.c
@@ -1,19 +1,36 @@
+/*
+ *@file SD.c
+ *@author Da Silva Andrade David, Antoine Berger, Dos Santos Rafael
+ *@version 1.0
+ *@date 19 June 2014
+ *@briefs Contain functions to init SD card, read blocs on SD card.
+ */
+
 #include "SD.h"
 int x, i, y, data, k, l = 0;
 uint8_t rep;
 int reponse[512];
 uint8_t Tab[512];
-
+/*
+ *@briefs Chip Select
+ */
 void Clear_pin() {
 	LPC_GPIO0->FIOCLR = 1 << 10;
 	Valide_datas_bus_to_extlab2();
 }
-
+/*
+ *@briefs Chip Select
+ */
 void Set_pin() {
 	LPC_GPIO0->FIOSET = 1 << 10;
 	Valide_datas_bus_to_extlab2();
 }
 
+/*
+ *@briefs Read a block on the SD card by sending CMD17 command on SPI
+ *@brief stock block on array
+ *@param n_block Is the block you want to read
+ */
 void Read_SD_one_block(uint32_t n_block) {
 
 	Select_control_bus();
@@ -21,7 +38,7 @@ void Read_SD_one_block(uint32_t n_block) {
 	rep = Write_Read_SPI_8bits(0xff);
 	Clear_pin();
 
-	//Envoie de la CMD17
+	//send CMD17 command
 	Write_only_SPI_8bits(0b01010001); //startBit + Transmission+ CM
 
 	uint32_t arg = n_block * 512;
@@ -50,7 +67,12 @@ void Read_SD_one_block(uint32_t n_block) {
 	}
 	Set_pin();
 }
-
+/*
+ *@briefs Read multiple blocks on SD card by using Read_SD_one_block function
+ *@brief chose an interval with a starting block and end block
+ *@param n_block_depart Starting block
+ *@param n_block_arrive End block
+ */
 void Read_SD_multi_block(uint32_t n_block_depart,uint32_t n_block_arrive) {
 	uint8_t offset = 0; int i = 0;
 	k = 0;
@@ -79,13 +101,16 @@ void Read_SD_multi_block(uint32_t n_block_depart,uint32_t n_block_arrive) {
 			pixel_to_next_page[i] = Tab[510 + i];
 	}
 }
-
+/*
+ *@briefs Write on SD card -----THIS FUNCTION DOESN'T WORK-----------
+ *@brief Function isn't necesary in our work
+ */
 void Write_SD() {
 
 	rep = Write_Read_SPI_8bits(0xff);
 	Clear_pin();
 
-	//Envoie de la CMD24
+	//Send CMD24 command
 	Write_only_SPI_8bits(0b01011000); //startBit + Transmission+ CM
 	Write_only_SPI_8bits(0b00000000); //argument
 	Write_only_SPI_8bits(0b00000000); //
@@ -111,32 +136,31 @@ void Write_SD() {
 }
 
 void ChipSetSelect() {
-
-	/*LPC_GPIO0 -> FIODIR |= 0b11 << 21;
-	 LPC_GPIO0 -> FIOSET = 0b11 << 21;*/
 	Select_control_bus();
 	Valide_datas_bus_to_extlab2();
-	/*LPC_GPIO2 ->FIODIR |= 0b1 << 8;
-	 LPC_GPIO2 ->FIOSET = 0b1 << 8;
-	 LPC_GPIO2 ->FIOCLR = 0b1 << 8;*/
 }
-
+/*
+ *@briefs SPI initialisation on SD card
+ *@brief Send different Command with SPI protocol on SD card 
+ *@brief Set SPI frequency for initialisation only
+ *@brief Protocol initialisation is describe step by step on our document (scheme)
+ */
 void init_SD() {
 	int x = 0;
 	int y = 0;
 	int i = 0;
 
-	int Tab[5] = { 0, 0, 0, 0, 0 };
+	int Tab[5] = { 0, 0, 0, 0, 0 }; // arry used for SPI return
 
 	ChipSetSelect();
 
-	Change_Frequency_SPI(400000);
-	//Init_SPI_master_mode(0, 0, 400000, 8);
+	Change_Frequency_SPI(400000); //Set SPI frequency for initialisation
+	
 
-	LPC_GPIO0->FIODIR |= 1 << 10; // met la pin en sortie
+	LPC_GPIO0->FIODIR |= 1 << 10; 
 	Clear_pin();
 
-	//80 coups d'horloges
+	//Waiting for 80 tick on SPI before sending command
 	Write_only_SPI_8bits(0xFF);
 	Write_only_SPI_8bits(0xFF);
 	Write_only_SPI_8bits(0xFF);
@@ -150,40 +174,40 @@ void init_SD() {
 
 	Set_pin();
 
-	// 24 coups d'horloges
+	//Waiting for 24 tick on SPI before sending command
 	Write_only_SPI_8bits(0xFF);
 	Write_only_SPI_8bits(0xFF);
 
-	//Envoie de la CMD0
+	//sending CMD0 command
 	Clear_pin();
 	Write_only_SPI_8bits(0b01000000); //startBit + Transmission+ CMD
 	Write_only_SPI_8bits(0b00000000); //argument
 	Write_only_SPI_8bits(0b00000000); //
 	Write_only_SPI_8bits(0b00000000); //
-	Write_only_SPI_8bits(0b00000000); //fin argument
+	Write_only_SPI_8bits(0b00000000); //argument end
 	Write_only_SPI_8bits(0b10010101); // CRC7 + endBit
 	do {
 		rep = Write_Read_SPI_8bits(0xFF);
-	} while (rep >> 7);
+	} while (rep >> 7); // check if bit 7 is OK
 
 	Set_pin();
 	rep = Write_Read_SPI_8bits(0xff);
 
-	//tant que IDLE <> 0 renvoyer les CMD
+	//while IDLE <> 0 send CMD
 	do {
 		Clear_pin();
 
-		//Envoie de la CMD55 pour pouvoir envoyer ACMD41
+		//send CMD55 command before sending ACMD41 command
 
 		Write_only_SPI_8bits(0b01110111); //startBit + Transmission+ CM
 		Write_only_SPI_8bits(0b00000000); //argument
 		Write_only_SPI_8bits(0b00000000); //
 		Write_only_SPI_8bits(0b00000000); //
-		Write_only_SPI_8bits(0b00000000); //fin argument
+		Write_only_SPI_8bits(0b00000000); //argument end
 		Write_only_SPI_8bits(0b00000001); // CRC7 + endBit
 		do {
 			rep = Write_Read_SPI_8bits(0xff);
-		} while (rep >> 7);
+		} while (rep >> 7); // check if bit 7 is OK
 
 		Set_pin();
 
@@ -191,16 +215,16 @@ void init_SD() {
 
 		Clear_pin();
 
-		//Envoie de la ACMD41
+		//sending ACMD41 command
 		Write_only_SPI_8bits(0b01101001); //startBit + Transmission+ CM
 		Write_only_SPI_8bits(0b00000000); //argument
 		Write_only_SPI_8bits(0b00000000); //
 		Write_only_SPI_8bits(0b00000000); //
-		Write_only_SPI_8bits(0b00000000); //fin argument
+		Write_only_SPI_8bits(0b00000000); //argument end
 		Write_only_SPI_8bits(0b00000001); // CRC7 + endBit
 		do {
 			rep = Write_Read_SPI_8bits(0xff);
-		} while (rep >> 7);
+		} while (rep >> 7); // check if bit 7 is OK
 
 		Set_pin();
 		Write_Read_SPI_8bits(0xff);
@@ -209,30 +233,27 @@ void init_SD() {
 
 	Clear_pin();
 
-	//Envoie CMD 58
+	//sending CMD58 command
 	Write_only_SPI_8bits(0b01111010); //startBit + Transmission+ CM
 	Write_only_SPI_8bits(0b00000000); //argument
 	Write_only_SPI_8bits(0b00000000); //
 	Write_only_SPI_8bits(0b00000000); //
-	Write_only_SPI_8bits(0b00000000); //fin argument
+	Write_only_SPI_8bits(0b00000000); //argument end
 	Write_only_SPI_8bits(0b00000001); // CRC7 + endBit
 
 	do {
 		rep = Write_Read_SPI_8bits(0xff);
-	} while (rep >> 7);
+	} while (rep >> 7); // check if bit 7 is OK
 	Tab[0] = x;
 	for (i = 1; i < 5; i++) {
-		x = Write_Read_SPI_8bits(0xff);
+		x = Write_Read_SPI_8bits(0xff); //Stock SPI return on array to check condition 
 		Tab[i] = x;
 	}
 
 	Set_pin();
-
 	y = Tab[2];
 
 	if ((y & (1 << 6)) | (y & (1 << 7))) {
-		Change_Frequency_SPI(5000000);
-		//Init_SPI_master_mode(0, 0, 5000000, 8);
+		Change_Frequency_SPI(5000000); // set frequency to the one you want
 	}
 }
-
